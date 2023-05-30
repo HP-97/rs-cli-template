d39c334e25248d0b8539293a2e0612004e3e1c03
HP97 <phamh.2014@gmail.com>
2023-05-31 01:49:27 +1000

    added test printlns

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,9 @@
-use rs_cli_template::{clap::parse_args};
+use rs_cli_template::{clap::parse_args, utils::config::AppConfig};
 
 fn main() {
     let m = parse_args();
+
+    let c = AppConfig::new(None);
     println!("{:?}", m);
+    println!("{:?}", c);
 }
