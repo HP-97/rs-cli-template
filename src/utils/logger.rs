da25eb502c2d28014ae86f7da7aca9f61b75ddb7
HP-97 <phamh.2014@gmail.com>
2023-06-11 23:25:04 +1000

    enable support for configurable log level

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,17 +1,16 @@
-use anyhow::Result;
 use time::macros::format_description;
 use tracing::Level;
 use tracing_subscriber::{fmt::{writer::MakeWriterExt, time::LocalTime, Layer}, prelude::__tracing_subscriber_SubscriberExt};
 
+use crate::error::AppError;
+
 // Sets up a global logger for the whole application
-pub fn setup_logging() -> Result<()> {
+pub fn setup_logging(log_level: Level) -> Result<(), AppError> {
 
-    // TODO: Get the max logging level
-    // let log_level = match 
+    // Documentation on implemention a Layer wrapped in an Option
+    // https://docs.rs/tracing-subscriber/latest/tracing_subscriber/layer/
 
-    // TODO: 
-    // Setup the subscriber for stdout
-    let stdout = std::io::stdout.with_max_level(Level::DEBUG);
+    let stdout = std::io::stdout.with_max_level(log_level);
     
     let time_format = LocalTime::new(format_description!("[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:6][offset_hour sign:mandatory]:[offset_minute]"));
 
