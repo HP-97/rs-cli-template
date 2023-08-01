40ae537115db9a177a1692a31c576a5f7c70ceed
HP97 <phamh.2014@gmail.com>
2023-08-01 23:56:22 +1000

    get rid of build warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 use std::path::PathBuf;
 
-use crate::prelude::*;
+// Uncomment as required
+// use crate::prelude::*;
 
 pub mod prelude;
 pub mod cli;
 pub mod error;
 pub mod utils;
 
-fn get_program_path(binary_name: &str) -> Option<PathBuf> {
+fn _get_program_path(binary_name: &str) -> Option<PathBuf> {
     match which::which(binary_name) {
         Ok(path) => Some(path),
         Err(_) => None,
