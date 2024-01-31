3e298c3e13c843c4fafa4aefc80e6e42ca38110b
hoa <Hoa.Pham@ue.com.au>
2024-01-31 13:46:41 +1100

    output logging to stderr rather than stdout

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -12,12 +12,12 @@ pub fn setup_logging(log_level: Level) -> Result<()> {
     // Documentation on implemention a Layer wrapped in an Option
     // https://docs.rs/tracing-subscriber/latest/tracing_subscriber/layer/
 
-    let stdout = std::io::stdout.with_max_level(log_level);
+    let stderr = std::io::stderr.with_max_level(log_level);
 
     let time_format = LocalTime::new(format_description!("[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:6][offset_hour sign:mandatory]:[offset_minute]"));
 
     let subscriber = tracing_subscriber::registry()
-        .with(Layer::new().with_writer(stdout).with_timer(time_format));
+        .with(Layer::new().with_writer(stderr).with_timer(time_format));
 
     tracing::subscriber::set_global_default(subscriber)
         .expect("failed to set global logging instance");
