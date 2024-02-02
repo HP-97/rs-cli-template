7f4d760968ba69782d3acecfc07b432bda82b329
hoa <Hoa.Pham@ue.com.au>
2024-02-02 13:14:03 +1100

    manually deal with errors in main.rs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,15 +2,24 @@ use crate::prelude::*;
 
 use rs_cli_template::{
     cli::parse_args,
-    utils::{config::AppConfig, logger},
     prelude,
+    utils::{config::AppConfig, logger},
 };
+use std::{io::IsTerminal, process::exit, str::FromStr};
 use tracing::Level;
-use std::{process::exit, str::FromStr, io::IsTerminal};
 
-fn main() -> Result<()> {
+fn main() -> Result<()>{
     let m = parse_args();
-    let cfg = AppConfig::new(&m)?;
+    let cfg = match AppConfig::new(&m) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "failed to create application configuration. REASON: {}",
+                e.to_string()
+            );
+            exit(1)
+        }
+    };
 
     if cfg.log_level > 0 {
         let log_level = match tracing::Level::from_str(&cfg.log_level.to_string()) {
@@ -20,13 +29,16 @@ fn main() -> Result<()> {
                 exit(1)
             }
         };
-        logger::setup_logging(log_level)?;
+        if let Err(e) = logger::setup_logging(log_level) {
+            eprintln!("failed to setup logging. REASON: {}", e.to_string());
+            exit(1)
+        };
     }
 
     tracing::event!(Level::DEBUG, "program START");
 
     // Below is an code example of working with stdin
-    if ! std::io::stdin().is_terminal() {
+    if !std::io::stdin().is_terminal() {
         let mut buf = String::new();
         if let Err(e) = std::io::stdin().read_line(&mut buf) {
             tracing::error!("{}", e.to_string());
